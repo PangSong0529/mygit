5f254cd306473998278f14fc3a566687330b2e45
PS <pangsong0529@163.com>
2019-03-07 21:32:22 +0800

    第一次提交

--- src/main/java/com/pangsong/testGit/App.java
+++ src/main/java/com/pangsong/testGit/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!1112222233333" );
+        System.out.println("Hello World!");
     }
 }
